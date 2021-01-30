62f1943a75ca7fc3985ad0727e0f970c7220f2a3
saso949 <arukimakurou2@gmail.com>
2021-01-30 17:02:19 +0900

    seekbarの%を表示する機能を追加

--- app/src/main/java/com/example/janken_app/MainActivity.kt
+++ app/src/main/java/com/example/janken_app/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.janken_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            // 値が変更された時に呼ばれる
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                parcentView.text = progress.toString() + "%"
+            }
+        })
+
     }
 }
