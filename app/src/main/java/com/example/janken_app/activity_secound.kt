86c8726dfb6ca7bdc6e16ec2357adc96754fcc49
saso949 <arukimakurou2@gmail.com>
2021-01-31 11:48:03 +0900

    secoundactivityの削除

--- app/src/main/java/com/example/janken_app/activity_secound.kt
+++ app/src/main/java/com/example/janken_app/activity_secound.kt
@@ -2,10 +2,20 @@ package com.example.janken_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_secound.*
 
 class activity_secound : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_secound)
+        Log.d("","aaaaaaa")
+
+        countSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+                Log.d("",isChecked.toString())
+
+        }
+
+
     }
 }
